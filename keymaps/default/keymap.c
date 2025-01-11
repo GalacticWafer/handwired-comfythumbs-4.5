e6f2039464ff80e9d2273150b21dee965ac2cdcb
GalacticWafer <159392305+galacticwafer@users.noreply.github.com>
2025-01-11 06:04:08 -0700

    Update files

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,20 +15,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_HOME, KC_PGDN, KC_END,           KC_TAB, KC_LCTL, KC_LALT,  /**/    KC_RALT, KC_RCTL,          KC_ENT, KC_LEFT,  KC_DOWN, KC_RIGHT
 ),
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    return true;
-}
