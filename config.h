5188d45756849de92f5548c15d4a94bd372936f6
MXHJohn314 <mxhjohnson4dba@gmail.com>
2025-01-11 15:59:06 -0700

    Update files

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
+
+/* mouse config */
+#define MOUSEKEY_INTERVAL    20
+#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_WHEEL_DELAY 0
+
+/* Enables This makes it easier for fast typists to use dual-function keys */
+#define PERMISSIVE_HOLD
